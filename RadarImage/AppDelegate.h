9a7a45c2a8fe9f0b8a1da50b3fefe1b81a9634de
Stefan Walkner <stefan@arkulpa.at>
2016-06-28 11:04:13 +0200

    Initial Commit

---
+++ RadarImage/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  RadarImage
+//
+//  Copyright © 2016 arkulpa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RadarImage/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  RadarImage
+//
+//  Copyright © 2016 arkulpa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
